461bb1d2dbe7cdb81a8a78ba528eab7c45c45267
Dylan Scott <scott.dylan@abc.net.au>
2019-02-22 11:56:09 +1100

    finish lambda upload

--- lambda.js
+++ lambda.js
@@ -15,17 +15,38 @@ exports.handler = async (event, context) => {
     let body;
 
     if(event.path === '/') {
-        let players = Player.loadPlayers(PLAYER_DIR);
+        let players = Player.loadFromDirectory(PLAYER_DIR);
         let page = React.createElement(MainPage, { players: players });
-        body = ReactDOMServer.renderToString(page);
+        return {
+            statusCode: 200,
+            statusDescription: '200 OK',
+            isBase64Encoded: false,
+            headers: { 'content-type': 'text/html' },
+            body: ReactDOMServer.renderToString(page),
+        };
     }
 
     if(event.path === '/match') {
-        let player1 = new Player(PLAYER_DIR, event.queryStringParameters.player1);
-        let player2 = new Player(PLAYER_DIR, event.queryStringParameters.player2);
+        let players = Player.loadFromDirectory(PLAYER_DIR);
+        let player1;
+        let player2;
+        players.forEach(player => {
+            if(player.name === event.queryStringParameters.player1) {
+                player1 = player;
+            }
+            if(player.name === event.queryStringParameters.player2) {
+                player2 = player;
+            }
+        });
         let match = new Match(player1, player2, event.queryStringParameters.count);
         let page = React.createElement(MatchPage, { match: match });
-        body = ReactDOMServer.renderToString(page);
+        return {
+            statusCode: 200,
+            statusDescription: '200 OK',
+            isBase64Encoded: false,
+            headers: { 'content-type': 'text/html' },
+            body: ReactDOMServer.renderToString(page),
+        };
     }
 
     if(event.path === '/upload') {
@@ -45,15 +66,12 @@ exports.handler = async (event, context) => {
             TableName: 'battleship',
         };
         await new Promise((resolve, reject) => dynamodb.putItem(params, (err, data) => err ? reject(err) : resolve(data)));
-        body = 'ok';
+        return {
+            statusCode: 303,
+            statusDescription: '303 See Other',
+            isBase64Encoded: false,
+            headers: { location: '/' },
+        };
     }
-
-    return {
-        statusCode: 200,
-        statusDescription: '200 OK',
-        isBase64Encoded: false,
-        headers: { 'content-type': 'text/html' },
-        body: body,
-    };
 
 };
